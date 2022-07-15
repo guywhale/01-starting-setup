de9ee5c051263c322baacdd07c57d2ed4a9732df
Guy Whale <guywhale@gmail.com>
2022-07-15 13:46:55 +0100

    Change component functions to arrow components because easier to use

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -2,7 +2,7 @@ import Card from "../UI/Card";
 import ExpenseItem from "./ExpenseItem";
 import "./Expenses.css";
 
-function Expenses(props) {
+const Expenses = (props) => {
   return (
     <Card className="expenses">
       {props.expenses.map((expense) => (
@@ -16,6 +16,6 @@ function Expenses(props) {
       ))}
     </Card>
   );
-}
+};
 
 export default Expenses;
