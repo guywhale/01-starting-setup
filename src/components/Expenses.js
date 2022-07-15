f58918efd58d6d0b963f1566b682757f6026d8a4
Guy Whale <guywhale@gmail.com>
2022-07-15 13:25:03 +0100

    Include a key for expenses component map function as this is required and was throwing a console error

--- src/components/Expenses.js
+++ src/components/Expenses.js
@@ -7,6 +7,8 @@ function Expenses(props) {
     <Card className="expenses">
       {props.expenses.map((expense) => (
         <ExpenseItem
+          key={expense.id}
+          id={expense.id}
           title={expense.title}
           amount={expense.amount}
           date={expense.date}
